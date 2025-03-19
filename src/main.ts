a46c16f8ba435a45a954ed01f7a0cdd452c863f4
MelanyPedemonte <melanypedemonte@gmail.com>
2025-03-18 22:40:10 -0300

    add origin cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Conexa Challenge')
